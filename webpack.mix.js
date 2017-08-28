c0b4f55461313287133b4f7ffd80b4e62c9faa98
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2017-08-27 22:36:13 -0400

    [ADD] Finalizando laravel mix
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,25 @@ let mix = require('laravel-mix');
 // mix.js('resources/assets/js/app.js', 'public/js')
   //  .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
-   .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/fonts/css/font-awesome.min.css')
+// mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
+  //  .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/fonts/css/font-awesome.min.css')
 
-  //  .copy('node_modules/font-awesome/fonts', 'public/fonts')
-  //  .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/fonts/css/font-awesome.min.css');
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
+   .copy('node_modules/bootstrap/dist/fonts', 'public/fonts');
+
+mix.styles([
+  'node_modules/font-awesome/css/font-awesome.min.css',
+  'node_modules/bootstrap/dist/css/bootstrap.min.css'
+], 'public/css/style.css');
+
+mix.scripts([
+  'node_modules/jquery/dist/jquery.min.css'
+], 'public/js/scripts.js');
+
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('resources/assets/js/app.js', 'public/js')
+  .sourceMaps()
+  .extract(['jquery', 'bootstrap']);
+
+  mix.browserSync('localhost:8000')
